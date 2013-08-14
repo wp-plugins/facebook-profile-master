feac6f386e53f970ec3ae7bce9eb584eaa53bed6
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-14 10:07:11 +0000

    version 2.2
    git-svn-id: https://plugins.svn.wordpress.org/facebook-profile-master/tags/2.2@756057 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ techgasp-facebookprofilemaster-admin.php
@@ -0,0 +1,53 @@
+<div class="wrap">
+<div class="icon32" style="width:40px; vertical-align:middle;"><img src="../wp-content/plugins/facebook-profile-master/techgasp-minilogo.png" alt="' . esc_attr__( 'TechGasp Plugins') . '" /><br /></div>
+
+<h2><b>TechGasp - Facebook Profile Master Lite Version</b></h2>
+<form method="post" action="techgasp-facebookprofilemaster-admin.php"> 
+<p><b>About TechGasp:</b></p>
+<p>Major player in the Content Management System world! Joomla and Jomsocial Partner with more than 100 high quality, error free Extensions. We provide website customizations and development, SEO Optimization, Facebook Apps, etc. We have fast & furious specialized Hosting for WordPress and Joomla. Our promise, the use of minimal error free code with fast loading times. Also to avoid the implementation of useless Javascript and Ajax in CMS by replacing it with solida php, css, html5 and iframe. Check our website for more high quality extensions. Stay up to date by "like" and "follow" our facebook and twitter page for fresh news, releases and upgrades and updates.</p>
+
+<p>
+<a class="button-primary" href="http://wordpress.techgasp.com" target="_blank" title="Visit Website">Wordpress Website</a> 
+<a class="button-primary" href="http://www.techgasp.com" target="_blank" title="Visit Website">Joomla Website</a>
+<a class="button-primary" href="http://hosting.techgasp.com" target="_blank" title="Visit Website">Hosting Website</a>
+<a class="button-secondary" href="https://www.facebook.com/TechGasp" target="_blank" title="Facebook Page">Facebook Page</a> 
+<a class="button-secondary" href="https://twitter.com/TechGasp" target="_blank" title="Follow Twitter">Twitter Page</a>
+<a class="button-secondary" href="https://plus.google.com/118126459543511361864" target="_blank" title="Follow Google">Google Page</a>
+</p>
+
+<h3>Stay up-to-date with new extension releases, extension updates, & upgrades:</h3>
+<p>
+<span style="float:left; vertical-align:baseline;">
+<fb:like href="https://www.facebook.com/TechGasp" send="true" layout="button_count" width="90" show_faces="false"></fb:like>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=281766848505812";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+&nbsp;
+<a href="https://twitter.com/TechGasp" class="twitter-follow-button" data-show-count="true">Follow @TechGasp</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+</span>
+</p>
+
+<hr>
+
+<img src="../wp-content/plugins/facebook-profile-master/techgasp-facebookprofilemaster-logo.png" alt="Facebook Profile Master" align="left" width="387px" height="171px" style="padding:5px;"/>
+<p><h3>Facebook Profile Master:</h3></p>
+<p>Facebook Profile Master gives you full control over all Facebook User Profile plugins, Facebook User Badges and Facebook Follow Button..</p>
+<p><em>Facebook User Profiles for Wordpress...</em><p>
+<p><h3>Options & Configuration:</h3></p>
+<p><b>All configuration options are available in your widgets page. <b>Facebook Profile Master Widget</b>:</b>.</p>
+<p>Select or <em>"add"</em > Facebook Profile Master Widget. Configure Widget. Select widget position according to your template. That easy...</p>
+<p><b>Insert Facebook Profile Master Widget inside Pages and Posts (advanced version only):</b></p>
+<p>Copy and Paste this shortcode inside your pages or posts [facebookprofilemaster id=xx] replace xx with your sidebar number, ie [facebookprofilemaster id=sidebar-1]</p>
+<p><a class="button-primary" href="widgets.php" title="Widgets Page">To Widgets Page</a> <a class="button-secondary" href="http://wordpress.techgasp.com/facebook-profile-master/" target="_blank" title="More Help">More Help</a> <a class="button-primary" href="http://wordpress.techgasp.com/facebook-profile-master/" target="_blank" title="Facebook Profile Master Advanced Version">Facebook Profile Master Advanced Version</a></p>
+<hr>
+<p><h3>Facebook Profile Master Advanced Version</h3></p>
+<p><b>You have the lite version. Upgrade now!!</b> Get full advanced options: * Display or hide Widget Title * Facebook Profile Badge * Facebook Photo Badge * Facebook Like Badge * Facebook Page Badge * Facebook Follow Button * Shortcode Framework to publish widget inside pages and posts.</p>
+<p><a class="button-primary" href="http://wordpress.techgasp.com/facebook-profile-master/" target="_blank" title="Facebook Profile Master Advanced Version">Facebook Profile Master Advanced Version</a></p>
+</form>
+</div>
